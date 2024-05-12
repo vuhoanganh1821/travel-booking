9c050b57eca485a1e15c618b454a5f33f8290506
Vu Hoang Anh <vuhoanganh1821@gmail.com>
2024-05-12 15:12:08 +0700

    Enhance CMS Login page

--- pages/CMS/LoginPage/index.tsx
+++ pages/CMS/LoginPage/index.tsx
@@ -4,15 +4,12 @@ import {
   Box,
   Button,
   Checkbox,
-  Container,
-  Divider,
   Heading,
   HStack,
-  Link,
   Stack,
   Text,
+  VStack,
 } from '@chakra-ui/react'
-import Icon from 'components/Icon'
 import FormInput from 'components/FormInput'
 import PasswordField from 'components/PasswordField'
 import { PLATFORM } from 'enums/common'
@@ -45,61 +42,50 @@ const LoginPage = () => {
   }
 
   return (
-    <Container maxW="lg" py={{ base: 12, md: 24 }} px={{ base: 0, sm: 8 }}>
-      <Stack spacing={8}>
-        <Stack spacing={6}>
-          <Stack spacing={{ base: 2, md: 3 }} textAlign="center">
-            <Heading size={{ base: 'xs', md: 'lg' }}>Log in to your account</Heading>
-            <Text color="fg.muted">
-              {`Don't have an account?`} <Link href="#">Sign up</Link>
-            </Text>
+    <Box width="100vw" margin={0}>
+      <HStack width="100vw">
+        <VStack width="1200px" height="100vh" justify="center" background="teal.400" padding={16} spacing={8}>
+          <Heading size={{ base: 'xs', md: 'xl' }} color="white">
+            Welcome Back!
+          </Heading>
+          <Text color="white" textAlign="center">
+            Welcome back! We are so happy to have you here. It's great to see you again. We hope you had a safe and enjoyable time away.
+          </Text>
+        </VStack>
+        <Stack width="70%" marginX="250px" spacing={8}>
+          <Stack spacing={6}>
+            <Stack spacing={{ base: 2, md: 3 }} textAlign="center">
+              <Heading size={{ base: 'xs', md: 'lg' }}>Login</Heading>
+            </Stack>
           </Stack>
-        </Stack>
-        <FormProvider {...methods}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Box bg={{ base: 'transparent', sm: 'bg.surface' }} borderRadius={{ base: 'none', sm: 'xl' }}>
-              <Stack spacing={6}>
-                <Stack spacing="5">
-                  <FormInput name="email" label="Email" autoComplete="off" />
-                  <PasswordField />
-                </Stack>
-                <HStack justify="space-between">
-                  <Checkbox defaultChecked {...register('isRemember')}>
-                    Remember me
-                  </Checkbox>
-                  <Button variant="text" size="sm">
-                    Forgot password?
-                  </Button>
-                </HStack>
+          <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Box bg={{ base: 'transparent', sm: 'bg.surface' }} borderRadius={{ base: 'none', sm: 'xl' }}>
                 <Stack spacing={6}>
-                  <Button type="submit" colorScheme="teal" isLoading={isLoading}>
-                    Sign in
-                  </Button>
-                  <HStack>
-                    <Divider borderColor="gray.300" />
-                    <Text fontSize="sm" whiteSpace="nowrap" color="fg.muted">
-                      OR
-                    </Text>
-                    <Divider borderColor="gray.300" />
+                  <Stack spacing="5">
+                    <FormInput name="email" label="Email" autoComplete="off" />
+                    <PasswordField />
+                  </Stack>
+                  <HStack justify="space-between">
+                    <Checkbox defaultChecked {...register('isRemember')}>
+                      Remember me
+                    </Checkbox>
+                    <Button variant="text" size="sm">
+                      Forgot password?
+                    </Button>
                   </HStack>
-                  <Button
-                    fontSize="sm"
-                    fontWeight={500}
-                    background="none"
-                    border="1px solid #CBD5E0"
-                  >
-                    <Icon iconName="google.svg" size={20} />
-                    <Text marginLeft={2} color="gray.700" lineHeight={6}>
-                      Continue with Google
-                    </Text>
-                  </Button>
+                  <Stack spacing={6}>
+                    <Button type="submit" colorScheme="teal" isLoading={isLoading}>
+                      Login
+                    </Button>
+                  </Stack>
                 </Stack>
-              </Stack>
-            </Box>
-          </form>
-        </FormProvider>
-      </Stack>
-    </Container>
+              </Box>
+            </form>
+          </FormProvider>
+        </Stack>
+      </HStack>
+    </Box>
   )
 }
 
